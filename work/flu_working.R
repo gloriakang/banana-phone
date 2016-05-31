62cec618e486cb7439acd2238a3572554aad6619
gkang <gkang@vt.edu>
2016-05-31 19:43:24 -0400

    update

--- work/flu_working.R
+++ work/flu_working.R
@@ -1,15 +1,36 @@
 # Flu survey data analysis
-
 setwd("~/git/banana-phone/work")
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
-data <- read.csv("surveydata.csv")
+data <- read.csv("surveydata.csv", na = "#NULL!")
 class(data)
-
-
-str(data)
 View(data)
 
-# subsetting with dplyr
-names(data)
+
+# subset question data
+q7.df <- data %>%
+  select(Q7_1:Q7_otherText) %>%
+  rename("Bus" = Q7_1,
+         "Carpool" = Q7_2,
+         "Subway" = Q7_3,
+         "Train" = Q7_4,
+         "Taxi" = Q7_5,
+         "Airplane" = Q7_6,
+         "Other" = Q7_7,
+         "Refused" = Q7_8,
+         "Other text" = Q7_otherText
+  )
+
+# gather into columns
+q7 <- q7.df %>%
+  gather("option", "response", 1:8)
+
+# show counts
+q7 %>%
+  group_by(response, option) %>%
+  summarise(count = n())
+
+
+
