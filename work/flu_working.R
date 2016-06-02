7eafafa06962dc82e2a2f9600fb4c263064f4398
gloriakang <gkang@vt.edu>
2016-06-01 22:45:38 -0400

    update

--- work/flu_working.R
+++ work/flu_working.R
@@ -6,11 +6,9 @@ library(tidyr)
 library(ggplot2)
 
 data <- read.csv("surveydata.csv", na = "#NULL!")
-class(data)
-View(data)
 
 
-# subset question data
+# select() question data
 q7.df <- data %>%
   select(Q7_1:Q7_otherText) %>%
   rename("Bus" = Q7_1,
@@ -24,14 +22,35 @@ q7.df <- data %>%
          "Other text" = Q7_otherText
   )
 
-# gather into columns
+# gather() into columns for plotting
 q7 <- q7.df %>%
-  gather("option", "response", 1:8)
+  gather("q", "r", 1:8)
 
-# show counts
+# group_by() and summarise() counts
 q7 %>%
-  group_by(response, option) %>%
+  group_by(r, q) %>%
   summarise(count = n())
+
+
+
+
+##### old test code #####
+
+q1.df = select(data, Q1)
+q1 = gather(q1.df, "q", "r", 1)
+summary(q1.df)
+
+q2.df = select(data, Q2)
+q2 = gather(q2.df, "option", "response", 1)
+summary(q2.df)
+
+q7.df = select(data, Q7_1:Q7_otherText)
+names(q7.df) = c("Bus", "Carpool", "Subway",
+                 "Train", "Taxi", "Airplane",
+                 "Other", "Refused", "Other text")
+q7 = gather(q7.df, "q", "r", 1:8)
+summary(q7.df)
+
 
 
 
