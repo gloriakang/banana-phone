2b5a6afb2830255fe7c77d2adbe83b32fbf6a0f8
gloriakang <gkang@vt.edu>
2016-06-28 10:46:45 -0400

    update

--- work/flu_current.R
+++ work/flu_current.R
@@ -4,6 +4,9 @@ library(tidyr)
 library(ggplot2)
 library(psych)
 
+data_W <- read.csv("data/surveydata.csv", na = c("#NULL!", "", "Refused"), stringsAsFactors = FALSE)
+data_UNW <- read.csv("data/surveydata_unw.csv", na = c("#NULL!", "", "Refused"), stringsAsFactors = FALSE)
+
 # source("data_cleaning.R")
 load("clean/all.Rdata")
 
@@ -15,4 +18,19 @@ names(data2) <- new_name
 with(data2, table(Q1))
 data2 %>%
   count(Q1)
+
+# unweighted Q1
+with(data_UNW, table(Q1))
+data_UNW %>%
+  count(Q1)
+
+
+# Q2
+with(data2, table(Q2))
+data2 %>%
+  count(Q2)
+
+with(data_UNW, table(Q2))
+data_UNW %>%
+  count(Q2)
 
