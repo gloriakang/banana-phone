4734640ea884261bd99470228a3ac6bc1a8f2310
gloriakang <gkang@vt.edu>
2016-06-27 20:10:22 -0400

    update

--- work/flu_current.R
+++ work/flu_current.R
@@ -1,1 +1,18 @@
-# 
+setwd("~/git/banana-phone/work")
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(psych)
+
+# source("data_cleaning.R")
+load("clean/all.Rdata")
+
+# set new names
+names(data2) <- new_name
+
+
+# Q1 tables
+with(data2, table(Q1))
+data2 %>%
+  count(Q1)
+
