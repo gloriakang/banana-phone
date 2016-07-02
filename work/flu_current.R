b524eb24055fca37fba433e0e7bc6cf83c89a141
Gloria Kang <gkang@vt.edu>
2016-07-02 19:01:51 -0400

    update

--- work/flu_current.R
+++ work/flu_current.R
@@ -1,11 +1,12 @@
 setwd("~/git/banana-phone/work")
+rm(list = ls())
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(psych)
 
-data_W <- read.csv("data/surveydata.csv", na = c("#NULL!", "", "Refused"), stringsAsFactors = FALSE)
-data_UNW <- read.csv("data/surveydata_unw.csv", na = c("#NULL!", "", "Refused"), stringsAsFactors = FALSE)
+#data_W <- read.csv("data/surveydata.csv", na = c("#NULL!", "", "Refused"), stringsAsFactors = FALSE)
+#data_UNW <- read.csv("data/surveydata_unw.csv", na = c("#NULL!", "", "Refused"), stringsAsFactors = FALSE)
 
 # source("data_cleaning.R")
 load("clean/all.Rdata")
@@ -13,6 +14,21 @@ load("clean/all.Rdata")
 # set new names
 names(data2) <- new_name
 
+
+#------- start --------#
+
+# get freq counts into a list where each element in a list is a dataframe
+lapply(X = data2[3:19], FUN = function(x){aggregate(data.frame(count = x), list(value = x), length)})
+
+
+
+
+
+
+
+
+
+# ----------------------#
 
 # Q1 tables
 with(data2, table(Q1))
@@ -37,7 +53,7 @@ data_UNW %>%
 
 
 
-#### Working analysis
+##### ----- Working analysis ----- #####
 
 # did you have the flu last year?
 with(data2, table(Q2))
@@ -71,57 +87,7 @@ sick2 <- sick %>%
 
 
 ########################## example plots ##############################
-# Q1
-with(data2, table(Q1))
-q1 <- data2 %>%
-  count(Q1)
 
-# plot
-ggplot(data2[!is.na(data2$Q1), ]) + geom_bar(mapping = aes(x = Q1, fill = Q1))
-
-# alternative plot:
-#ggplot(q1[!is.na(q1$Q1), ], aes(x = Q1, y = n, fill = Q1)) +
-#  geom_bar(stat = 'identity', position = position_dodge())
-
-
-
-# by gender, PPGENDER
-with(data2, table(PPGENDER, Q1))
-q1 <- data2 %>%
-  count(Q1, PPGENDER)
-
-# plot with facet
-ggplot(data2[!is.na(data2$Q1), ]) + geom_bar(mapping = aes(x = Q1, fill = Q1), position = position_dodge()) + facet_wrap(~PPGENDER)
-
-# plot with facet
-#ggplot(q1[!is.na(q1$Q1), ], aes(x = Q1, y = n, fill = Q1)) +
-#  geom_bar(stat = 'identity', position = position_dodge()) + facet_wrap(~PPGENDER)
-
-
-
-# by ethnicity, PPETHM
-with(data2, table(PPETHM, Q1))
-q1 <- data2 %>%
-  count(Q1, PPETHM)
-
-# plot
-#ggplot(q1[!is.na(q1$Q1), ], aes(x = Q1, y = n, fill = PPETHM)) +
-#  geom_bar(stat = 'identity', position = position_dodge())
-ggplot(data2[!is.na(data2$Q1), ]) + geom_bar(mapping = aes(x = Q1, fill = PPETHM), position = position_dodge())
-
-# plot with facet
-ggplot(data2[!is.na(data2$Q1), ]) + geom_bar(mapping = aes(x = Q1, fill = Q1), position = position_dodge()) + facet_wrap(~PPETHM)
-
-############################################################
-
-
-
-
-# use this one
-ggplot(data2[!is.na(data2$Q1), ]) + geom_bar(mapping = aes(x = Q1, fill = Q1))
-
-ggplot(q1[!is.na(q1$Q1), ], aes(x = Q1, y = n, fill = Q1)) +
-  geom_bar(stat = 'identity', position = position_dodge())
 
 
 # Q1 proportions
@@ -151,7 +117,7 @@ with(data2, table(Q2, Q13))
 # perceived risk
 with(Q11, table(q, r))
 
-# high risk 
+# high risk
 risk <- Q11 %>%
   filter(r=='High Risk, Very Likely')
 
@@ -192,6 +158,8 @@ table(testq2, testq3)
 summary(table(testq2, testq3))
 
 
-### using psych package ###
+
+
+########### using psych package ##############
 a <- describe(data2)
 View(a)
